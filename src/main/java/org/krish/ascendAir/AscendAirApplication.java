0dadc34036f1454477c68a976a3cff83572fe204
Krishna-Gokul <krishnagokul.go@gmail.com>
2024-02-23 13:36:13 +0530

    renamed the folder name

--- src/main/java/org/krish/ascendAir/AscendAirApplication.java
+++ src/main/java/org/krish/ascendAir/AscendAirApplication.java
@@ -1,4 +1,4 @@
-package org.ascendAir;
+package org.krish.ascendAir;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/org/krish/ascendAir/AscendAirApplicationTests.java
+++ src/test/java/org/krish/ascendAir/AscendAirApplicationTests.java
@@ -1,4 +1,4 @@
-package org.ascendAir;
+package org.krish.ascendAir;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
