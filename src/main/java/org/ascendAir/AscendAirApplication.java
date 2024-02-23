31faa72381ebeec233f117fc722a210a1770ac74
Krishna-Gokul <krishnagokul.go@gmail.com>
2024-02-23 13:20:30 +0530

    Added the base folder

---
+++ src/main/java/org/ascendAir/AscendAirApplication.java
@@ -0,0 +1,13 @@
+package org.ascendAir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AscendAirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AscendAirApplication.class, args);
+	}
+
+}
